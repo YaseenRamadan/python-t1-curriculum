605a981ebe5ca7d95fceafbe1f0bee32255e6894
YaseenRamadan <yaseen.sea@gmail.com>
2026-02-20 21:42:08 -0800

    Homerwork 1 completed

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,29 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+luckynumber = 5
+print(luckynumber)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
 
-
-
+hoursIslept= 9
+print(hoursIslept)
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
 
-
-
+favoritefruit = "mango"
+print("I like", favoritefruit)
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+mycity = "seattle"
+print(mycity)
+mycountry = "UnitedStates"
+print(mycountry)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+petsname="doug"
+petsage=6
+petstype="dog"
+print(petsname, "is", petsage,"and he is a", petstype)
