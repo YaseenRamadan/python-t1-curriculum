8d16e08af5c298b83ff4edcef52690d8fbd5e20a
YaseenRamadan <yaseen.sea@gmail.com>
2026-02-21 18:01:52 -0800

    lesson 2 completed

--- lesson2/classwork/math_operators.py
+++ lesson2/classwork/math_operators.py
@@ -1,10 +1,11 @@
-print(4+5) #addition: adds two 
+print(4+5) #addition: adds two numbers
 
 print(10-3) #subtraction: subtracts the second number from the first number
 
 print(6*7) #multiplacation: multiplies two numbers
 
-print(5/ 2) #division: divides two numbers and gives a float
+print(5/ 2) #division: divides two numbers and gives a float result
+
 a =5
 b=8
 print("a+b=", a + b)
